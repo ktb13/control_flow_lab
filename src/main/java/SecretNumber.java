29cbe6923e08b57dfed9b49b255cd05d6083cd25
K Bhayat <kbhayat@Ks-MacBook-Air.local>
2022-08-11 19:45:45 +0100

    completed tasks

--- src/main/java/SecretNumber.java
+++ src/main/java/SecretNumber.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
+
 public class SecretNumber {
 
     public static void main(String[] args) {
+        System.out.println("Guess the secret number");
+
+        Scanner reader = new Scanner(System.in);
+
+
+        int secretNumber = reader.nextInt();
+
+        if (secretNumber > 13) {
+
+            System.out.println("Too high");
+
+        } else if (secretNumber == 13) {
+
+            System.out.println("Correct!");
+
+
+        } else {
+            System.out.println("Too low");
+
+
+        }
 
     }
 
-
 }
+
--- src/main/java/WhatToWear.java
+++ src/main/java/WhatToWear.java
@@ -1,6 +1,38 @@
+import java.util.Scanner;
+
 public class WhatToWear {
 
     public static void main(String[] args) {
-        int secretNumber = 13;
+
+        System.out.println("What is the weather like?");
+
+        Scanner reader = new Scanner(System.in);
+        String whatsWeatherLike = reader.nextLine();
+
+        System.out.println("What is the temperature?");
+        int stateTemperature = reader.nextInt();
+
+        if (whatsWeatherLike.equals("rainy")) {
+
+            System.out.println("take brolly");
+
+        } else if (whatsWeatherLike.equals("sunny")) {
+
+            System.out.println("wear fave t-shirt");
+
+        } else if (whatsWeatherLike.equals("sunny") && stateTemperature == 27) {
+
+            System.out.println("put your fave dress on and go dance in the rain");
+
+            {
+
+
+            }
+
+
+        }
+
+
     }
+
 }
